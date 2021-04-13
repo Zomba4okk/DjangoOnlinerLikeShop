5f72f9429cf9d23c0bb5886d33695f904971baeb
Borisoid <boris.lazarev90@mail.ru>
2021-04-13 20:17:48 +0300

    Add News meta; Implement NewsAdmin

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,13 +1,47 @@
 from django.contrib import admin
-
-from .models import (  # noqa
-    News,
-    EditingHistoryEntry,
+from django.utils.html import (
+    format_html,
 )
+
+from .models import (
+    EditingHistoryEntry,
+    News,
+    NewsImage,
+)
+
+
+class EditingHistoryEntryInline(admin.TabularInline):
+    model = EditingHistoryEntry
+    readonly_fields = ('date_time', 'editor')
+    extra = 0
+
+    def has_add_permission(*args, **kwargs) -> bool:
+        return False
+
+    def has_delete_permission(*args, **kwargs) -> bool:
+        return False
+
+    def has_change_permission(*args, **kwargs) -> bool:
+        return False
+
+
+class NewsImageInline(admin.StackedInline):
+    model = NewsImage
+    fields = ('image', 'image_tag',)
+    readonly_fields = ('image_tag',)
+    extra = 1
+
+    def image_tag(self, obj):
+        return format_html('<img src="{url}"/>',
+                           url=obj.image.url)
 
 
 class NewsAdmin(admin.ModelAdmin):
     model = News
+    inlines = (
+        NewsImageInline,
+        EditingHistoryEntryInline
+    )
 
 
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
--- apps/news/models.py
+++ apps/news/models.py
@@ -18,13 +18,32 @@ class News(models.Model):
     content = HTMLField()
     main_image = models.ImageField(upload_to='news_images/')
 
+    def __str__(self):
+        return f'News article: {self.title}'
+
 
 class NewsImage(models.Model):
-    news = models.ForeignKey(to=News, on_delete=models.CASCADE)
+    news = models.ForeignKey(
+        to=News, on_delete=models.CASCADE,
+        related_name='images'
+    )
     image = models.ImageField(upload_to='news_images/')
 
 
 class EditingHistoryEntry(models.Model):
-    news = models.ForeignKey(to=News, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = 'editing history entries'
+
+    news = models.ForeignKey(
+        to=News, on_delete=models.CASCADE,
+        related_name='editing_history_enteries'
+    )
     date_time = models.DateTimeField()
-    editor = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    editor = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL, null=True,
+        related_name='news_edits'
+    )
+
+    def __str__(self):
+        return f'Edited by {self.editor.email} at ' + \
+               f'{self.date_time.__format__("%H:%M %d.%m.%Y")}'
